95f23fb44c0f783693387933d8cd89601cdcec79
ikdimkpa <ikennadimkpa@gmail.com>
2023-03-15 15:35:31 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,4 +6,4 @@ def square_matrix_simple(matrix=[]):
         for j in range(len(matrix[0])):
             new_matrix[i][j] = matrix[i][j] * matrix [i][j]
 
-    return (new_matrix)
+    return new_matrix
