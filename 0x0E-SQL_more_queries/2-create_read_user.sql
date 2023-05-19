4863fbc4c19c76b8ce29f07ef75759ddf1508495
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-05-19 04:15:26 -0700

    0x0E-SQL_more_queries_tasks_1_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates the MySQL server user user_0d_1.
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
-GRANT ALL PRIVILEGES ON 'user_0d_2' TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON 'user_0d_2'.* TO 'user_0d_2'@'localhost';
