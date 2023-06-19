68ae7cf654731407ee77fc402e9fc7221908d852
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-06-19 12:52:20 -0700

    1-filter_states.py_debugged

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
+"""Module to connect to MySQL Database and run a query"""
 import MySQLdb
 import sys
 
-# Get the MySQL username, password, and database name from command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-# Connect to the MySQL server
 if __name__ == '__main__':
-    connect_db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-            )
-
-# Create a cursor object to execute SQL queries
-cursor = connect_db.cursor()
-
-# Execute the query to retrieve all states
-query = "SELECT * FROM states ORDER BY id"
-cursor.execute(query)
-
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
-
-# Print the states in the desired format
-for row in rows:
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-connect_db.close()
+    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           password=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    connect.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
+"""Module to select rows with name starting with N"""
+
 import MySQLdb
 import sys
 
-# Get the MySQL username, password, and database name from command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-# Connect to the MySQL server
 if __name__ == '__main__':
-    connect_db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            b=database
-            )
-
-# Create a cursor object to execute SQL queries
-cursor = connect_db.cursor()
-
-# Execute the query to retrieve states starting with 'N'
-query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
-cursor.execute(query)
-
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
-
-# Print the states in the desired format
-for row in rows:
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-connect_db.close()
+    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           password=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = connect.cursor()
+    query = "SELECT * FROM states WHERE `name` LIKE BINARY" \
+            "'N%' ORDER BY id ASC"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    connect.close()
