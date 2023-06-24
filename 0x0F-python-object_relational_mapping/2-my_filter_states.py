a836ed712fea0b78993404b79d1c0b5116663a10
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-06-23 17:35:27 -0700

    2-my_filter_states.py_debugged

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
+"""Module to collect filter input"""
+
 import MySQLdb
 import sys
 
-# Get the MySQL username, password, database name, and state name from command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
-
-# Connect to the MySQL server
 if __name__ == '__main__':
-    connect_db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-            )
-
-# Create a cursor object to execute SQL queries
-cursor = connect_db.cursor()
-
-# Execute the query to retrieve states matching the input name
-query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-cursor.execute(query, (state_name,))
-
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
-
-# Print the states in the desired format
-for row in rows:
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-connect_db.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * " \
+            "FROM states " \
+            "WHERE name " \
+            "LIKE BINARY '{}' " \
+            "ORDER BY states.id ASC".format(sys.argv[4])
+    cur.execute(query)
+    query_all_rows = cur.fetchall()
+    for row in query_all_rows:
+        print(row)
+    cur.close()
+    conn.close()
