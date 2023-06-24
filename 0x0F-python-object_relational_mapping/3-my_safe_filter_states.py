b8f6a2fb09c74265d5483cd4fa53756d69092640
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-06-23 17:42:19 -0700

    3-my_safe_filter_states.py_debugged

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,8 @@ import sys
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    sql_injection = sys.argv[4].find(';')
-    if aql_injection == -1:
+    injection = sys.argv[4].find(';')
+    if injection == -1:
         cur = conn.cursor()
         query = "SELECT * " \
                 "FROM states " \
