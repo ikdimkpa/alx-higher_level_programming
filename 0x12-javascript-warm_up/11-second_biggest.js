c2af2822ed6bfb064c967f4fe4ce2fb6982f3f9c
root <root@ubuntu20.04.6.myguest.virtualbox.org>
2023-08-15 03:12:48 +0100

    0x12-javascript-warm_up task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (arg.length <= 1) {
+if (args.length <= 1) {
   console.log(0);
 } else {
-  const numbers = arg.map(x => parseInt(x));
+  const numbers = args.map(x => parseInt(x));
   const max = Math.max(...numbers);
   const secondMax = Math.max(...numbers.filter(x => x < max));
   console.log(secondMax);
