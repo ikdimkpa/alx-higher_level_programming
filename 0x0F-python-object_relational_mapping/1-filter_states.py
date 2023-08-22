b66bf2416a37aebd757d8e905ade15c711075e90
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-08-22 09:22:52 -0700

    1-filter_states.py completed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""A python script that lists all states with a name starting with N
+   from the database hbtn_0e_0_usa
+"""
 
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor
+    cursor.execute("SELECT * FROM states WHERE BINARY NAME LIKE 'N%';")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
