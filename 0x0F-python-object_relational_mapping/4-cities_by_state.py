1a07ffa2c57dd1e4692a2bf71368727b4b426091
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-08-22 12:46:22 -0700

    debugging 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""Python script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT c.id, c.name, s.name FROM cities as c inner join states as s\
             ON c.state_id = s.id")
