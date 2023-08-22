cca07a2286225796edd7a8d6583bf2b299551aab
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-08-22 12:29:16 -0700

    4-cities_by_state.py completed!

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+""""""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s\
+            ON c.state_id = s.id")
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cursor.close()
+    db.close()
 
