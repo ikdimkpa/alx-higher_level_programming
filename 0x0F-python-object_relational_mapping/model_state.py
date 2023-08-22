c1af6995644a29d351222c740f9df19aa3919537
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-08-22 08:56:13 -0700

    Debugged model_states.py and set all python files permission to executible

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""" Defines a class named State
+    Inherits from SQLAlchemy Base and links to the table states of MySQL
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """Contains the class definition of a State and an
