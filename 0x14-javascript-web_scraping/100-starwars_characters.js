45771ff9487ba3449f6fe857aca3c94e77929f1c
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-09-27 21:56:30 +0000

    100-starwars_characters.js successfully debugged!

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,14 +8,19 @@ request(url, (err, res, data) => {
   if (err) {
     console.log(err);
   } else {
-	  const actorsList = JSON.parse(data).characters;
-	  actorsList.forEach((actor) => {
-		  request(actor, (err, res, body) => {
-			  if (err) {
-				  console.log(err);
-				  return;
-				  }
-			  console.log(JSON.parse(body).name);
-		});
-	});
-}});
+    const actorsList = JSON.parse(data).characters;
+    actorsList.forEach((actor) => {
+      request(actor, (err, res, body) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        try {
+          console.log(JSON.parse(body).name);
+        } catch (error) {
+          console.log('Error parsing JSON:', error.message);
+        }
+      });
+    });
+  }
+});
