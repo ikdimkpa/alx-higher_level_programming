cded93fe58a404d61bdb34a15e94a8c12420117f
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-09-27 17:32:16 +0000

    succesfully debugged! task5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,16 +3,17 @@
 const fs = require('fs');
 const request = require('request');
 
-const url = process.argv[2]
+const url = process.argv[2];
 const pathToFile = process.argv[3];
 
-request({url: url, encoding: 'utf-8'}, (err, res, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-fs.writeFile(pathToFile, data, (err) => {
+request({ url, encoding: 'utf-8' }, (err, res, data) => {
   if (err) {
     console.log(err);
+    return;
   }
-})});
+  fs.writeFile(pathToFile, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
